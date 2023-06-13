151c14799db42a6bb5b5af0d88752c11443ea418
Kevin Tursi <kevin.tursi@uconn.edu>
2023-06-13 18:17:39 -0400

    Added MealItemForm.js and Input.js with styling

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import MealItemForm from "./MealItemForm";
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
@@ -12,6 +13,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
+                <MealItemForm />
             </div>
         </li>
     );
