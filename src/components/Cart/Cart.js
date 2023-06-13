e78fac06547f4067735d8d14b15eb604d6abdac0
Kevin Tursi <kevin.tursi@uconn.edu>
2023-06-13 18:38:33 -0400

    Added Modal.js and hardcoded data to ensure it works

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import classes from './Cart.module.css';
+import Modal from "../UI/Modal";
 
 const Cart = (props) => {
     const cartItems = <ul>{[{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map((item) => (
@@ -8,7 +9,7 @@ const Cart = (props) => {
     ))}</ul>;
 
     return (
-        <div>
+        <Modal>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
@@ -18,7 +19,7 @@ const Cart = (props) => {
                 <button className={classes['button--alt']}>Close</button>
                 <button className={classes.button}>Order</button>
             </div>
-        </div>
+        </Modal>
     );
 };
 
