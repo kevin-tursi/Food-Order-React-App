5866a4145b809f634ed013daff212d5c0445b95f
Kevin Tursi <kevin.tursi@uconn.edu>
2023-06-20 17:04:10 -0400

    using useRef to forward adding Items to Cart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 
+import CartContext from "../../../store/cart-context";
 import MealItemForm from "./MealItemForm";
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
+    const cartCtx = useContext(CartContext);
+
     const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = (amount) => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        })
+    };
+
     return (
         <li className={classes.meal} key={props.key}>
             <div>
@@ -13,7 +26,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm id={props.id} />
+                <MealItemForm onAddToCart={addToCartHandler} />
             </div>
         </li>
     );
