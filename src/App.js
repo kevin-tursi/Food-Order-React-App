95f957bc7d7411f6b41da90f6d0e1e1da77fcc23
Kevin Tursi <kevin.tursi@uconn.edu>
2023-06-20 15:56:15 -0400

    Added Cart Context to use throughout entire application

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -16,14 +17,14 @@ function App() {
   }
 
   return (
-    <>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCardHandler} />}
       <Header onShowCart={showCardHandler} />
       <main>
         <Meals />
       </main>
       
-    </>
+    </CartProvider>
   );
 }
 
---
+++ src/store/CartProvider.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const CartProvider = (props) => {
+    const addItemToCartHandler = (item) => {};
+
+    const removeItemFromCartHandler = (id) => {};
+
+    const CartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler
+    };
+
+    return (
+        <CartContext.Provider value={CartContext}>
+            {props.children}
+        </CartContext.Provider>
+    );
+};
+
+export default CartProvider;
